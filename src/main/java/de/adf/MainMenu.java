c5f03d39eaf1bfe50424aa65844bf30979b732f9
Andre_Martens <martens.andre92@gmail.com>
2020-05-20 14:34:50 +0200

    Anpassungen MainMenu

--- src/main/java/de/adf/MainMenu.java
+++ src/main/java/de/adf/MainMenu.java
@@ -1,32 +1,56 @@
 package de.adf;
 import java.awt.*;
 import javax.swing.*;
-public class MainMenu extends JFrame {
+import java.awt.event.*;
+public class MainMenu extends JFrame   {
     
     /**
      * Erzeugt das Hauptmenü des Spiels, mit allen Buttons.
      */
     public MainMenu() {
         setTitle("Schiffe versenken");
-        GridBagConstraints constraints = new GridBagConstraints();
         setLayout(new GridBagLayout());
-        setSize(600,500);
+        setSize(1000,700);
+        setLocationRelativeTo(null);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        constraints.insets = new Insets(0,0,10,0);
+
+        String resource = "PFad";
+        JLabel backgroundImage;
+        ImageIcon iconShips;
+        iconShips = new ImageIcon(getClass().getResource(resource));
+        backgroundImage = new JLabel(iconShips);
+        backgroundImage.setBounds(0, 0, 1000, 562);
+        add(backgroundImage);
+
         setVisible(true);
 
-        JButton btnStart = new JButton("Spielen");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+        final GridBagConstraints constraints = new GridBagConstraints();
+        constraints.insets = new Insets(0, 0, 10, 0);
+
+        final JButton start_btn = new JButton("Play");
+        //constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 0;
         constraints.gridy = 0;
-        getContentPane().add(btnStart, constraints);
+        getContentPane().add(start_btn, constraints);
+        start_btn.addActionListener(e -> startClicked(e));
 
-        JButton btnExit = new JButton("Beenden");
-        constraints.fill = GridBagConstraints.HORIZONTAL;
+        final JButton close_btn = new JButton("Close");
+        //constraints.fill = GridBagConstraints.HORIZONTAL;
         constraints.gridx = 0;
         constraints.gridy = 1;
-        getContentPane().add(btnExit, constraints);
+        getContentPane().add(close_btn, constraints);
+        close_btn.addActionListener(e -> closeClicked(e));
     }
-    
+
+    private void startClicked(ActionEvent e) {
+        new LobbyWindow();
+        setVisible(false);
+    }
+
+    private void closeClicked(ActionEvent e) {
+        setVisible(false);
+        dispose();
+        System.exit(0);
+    }
 
 }
