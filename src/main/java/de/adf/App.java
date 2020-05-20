d8484f575b7bf2b282dceb70b86029ff90c923a9
Sharagran-PC <filipmaas@googlemail.com>
2020-05-20 12:42:21 +0200

    getGewinner

--- src/main/java/de/adf/App.java
+++ src/main/java/de/adf/App.java
@@ -14,7 +14,7 @@ public final class App {
     public static void main(String[] args) {
         System.out.println("Hello World!");
         //new MainMenu();
-        new GameWindow();
-        //new LobbyWindow();
+        // new GameWindow();
+        new LobbyWindow();
     }
 }
--- src/main/java/de/adf/GameManager.java
+++ src/main/java/de/adf/GameManager.java
@@ -1,9 +1,35 @@
 package de.adf;
 
 public class GameManager {
+
+    private int[][] cells = new int[10][10];
+
     public GameManager() {
         super();
     }
 
-    //TODO: logic goes here
+    private int getWinner() {
+        int shipcountPlayer1 = 0;
+        int shipcountPlayer2 = 0;
+
+        for (int i = 0; i < cells.length; i++) {
+            for (int j = 0; j < cells.length; j++) {
+                int element = cells[i][j];
+
+                if (element == 1)
+                    shipcountPlayer1++;
+                else if (element == 2)
+                    shipcountPlayer2++;
+            }
+        }
+
+        if (shipcountPlayer1 == 0)
+            return 2;
+        else if (shipcountPlayer2 == 0)
+            return 1;
+        else
+            return 0;
+
+    }
+
 }
