61d0abbbdf28500cecf1e2868526eff6e74dd6dd
Sharagran-PC <filipmaas@googlemail.com>
2020-05-27 09:23:14 +0200

    comment & structure improvments

--- src/main/java/de/adf/GameManager.java
+++ src/main/java/de/adf/GameManager.java
@@ -5,12 +5,15 @@ import java.rmi.server.*;
 
 interface GameManagerInterface extends Remote {
     public boolean shoot(int x, int y) throws RemoteException;
+
     public boolean isLost() throws RemoteException;
 }
 
 public class GameManager extends UnicastRemoteObject implements GameManagerInterface {
-    //TODO: change GameManager into Multiplayer interface
-    //TODO: each GameManager only has the local player board. Local methods (placeShip, placeShipPart, getWinner[modified]), multiplayer methods (shoot, getWinner[modified])
+    // TODO: change GameManager into Multiplayer interface
+    // TODO: each GameManager only has the local player board. Local methods
+    // (placeShip, placeShipPart, getWinner[modified]), multiplayer methods (shoot,
+    // getWinner[modified])
 
     private int[][] myBoard = new int[10][10];
 
@@ -24,11 +27,11 @@ public class GameManager extends UnicastRemoteObject implements GameManagerInter
     // leftmost x, topmost y
     public boolean placeShip(int x, int y, int shipLenght, Boolean horizontal) {
         // Out of bound check
-        if(horizontal) {
-            if(x + shipLenght >= 10)
+        if (horizontal) {
+            if (x + shipLenght >= 10)
                 return false;
         } else {
-            if(y + shipLenght >= 10)
+            if (y + shipLenght >= 10)
                 return false;
         }
 
@@ -36,7 +39,7 @@ public class GameManager extends UnicastRemoteObject implements GameManagerInter
         for (int i = 0; i < shipLenght; i++) {
             placeShipPart(x, y);
 
-            if(horizontal)
+            if (horizontal)
                 x++;
             else
                 y++;
@@ -46,27 +49,27 @@ public class GameManager extends UnicastRemoteObject implements GameManagerInter
         return true;
     }
 
-    // Remote methode
+    private void placeShipPart(int x, int y) {
+        myBoard[x][y] = 1;
+    }
+
+    ////#region Remote methods
     public boolean shoot(int x, int y) throws RemoteException {
         boolean shipHit = myBoard[x][y] == 1;
         myBoard[x][y] = 0;
         return shipHit;
     }
 
-    private void placeShipPart(int x, int y) {
-        myBoard[x][y] = 1;
-    }
-
-    // Remote methode
     public boolean isLost() throws RemoteException {
         for (int i = 0; i < 10; i++) {
             for (int j = 0; j < 10; j++) {
-                if(myBoard[i][j] == 1)
-                    return false;   //Schiff gefunden = noch nicht verloren
+                if (myBoard[i][j] == 1)
+                    return false; // Schiff gefunden = noch nicht verloren
             }
         }
 
-        return true;    // kein Schiff gefunden = verloren
+        return true; // kein Schiff gefunden = verloren
     }
-
+    //#endregion
+    
 }
